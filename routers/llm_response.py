a70893a2a6c8c566da0d9870077a967f91c08ca2
Kamal Khan <kamal.ijk007@gmail.com>
2026-03-13 01:36:36 +0500

    API error handler

--- routers/llm_response.py
+++ routers/llm_response.py
@@ -2,15 +2,20 @@ from google import genai
 import os
 from dotenv import load_dotenv
 
+
 load_dotenv()
 
+
+api_key = os.getenv("API_KEY")
+client = genai.Client(api_key=api_key)
+
 def ask_gemini(question: str, context: str) -> str:
-    client = genai.Client(api_key=os.getenv("API_KEY"))
-    print(f"DEBUG: API Key found? {api_key is not None}")
+    print(f"DEBUG: API Key was found: {api_key is not None}")
+    
     prompt = f"Answer the question based on this context:\n\n{context}\n\nQuestion: {question}"
+    
     response = client.models.generate_content(
         model="gemini-1.5-flash",
         contents=prompt
     )
     return response.text
-
